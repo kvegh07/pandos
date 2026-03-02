eb09e8313030b3105976de0de76fc0a8f59976ed
PokLaszlo <pokica6@gmail.com>
2026-03-02 19:59:19 +0100

    A read művelet kész

---
+++ src/main/java/com/example/Solution.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void task01 () {
+        
+        DataSource dataSource = new DataSource(new Sqlite());
+        Product product = new Product();
+        product.name = "egér";
+        product.price = 30;
+        dataSource.createProduct(product);
+
+    }
+    public static void task02() {
+        DataSource dataSource = new DataSource(new Sqlite());
+        ArrayList<Product> productList = dataSource.readProducts();
+        for(Product product : productList) {
+            System.out.println(
+                product.name + " " +
+                product.price
+            );
+        }
+
+    }
+}
